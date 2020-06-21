54869f6ea4fe2be44dfcdfee8e83d50313dd6618
DESKTOP-VP7DCAG\rudi <shqdnix1112@gmail.com>
2020-06-21 10:24:13 +0200

    HomeActivityEdit

--- app/src/main/java/com/example/bankapplication/HomeActivity.kt
+++ app/src/main/java/com/example/bankapplication/HomeActivity.kt
@@ -5,18 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
 class HomeActivity : Fragment(){
 
-   // lateinit var comm: Communicator
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.content_home, container, false)
     }
 
@@ -28,19 +27,15 @@ class HomeActivity : Fragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        var saldoValue = arguments!!.getDouble("credit")
-//        if(!saldoValue.equals(null)) {
-//            var saldoText = view.findViewById<TextView>(R.id.textView_saldo)
-//            var saldo = saldoText.text.toString().toDouble() + saldoValue
-//            saldoText.text = saldo.toString()
-//        }
-
+//
         view.findViewById<Button>(R.id.button_logout).setOnClickListener {
             findNavController().navigate(R.id.action_homeActivity_to_loginActivity)
         }
 
         view.findViewById<Button>(R.id.button_make_transfer).setOnClickListener {
-            findNavController().navigate(R.id.action_homeActivity_to_transferActivity)
+            val valueSaldo = view.findViewById<TextView>(R.id.textView_saldo).text.toString().toFloat()
+            val action = HomeActivityDirections.actionHomeActivityToTransferActivity()
+            findNavController().navigate(action)
         }
 
         view.findViewById<Button>(R.id.button_credit).setOnClickListener {
